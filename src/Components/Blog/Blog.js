73ab2eb9b6114b138a3dd21967ed3393fe8323d2
MasrulSakib <Masrul.Sakib69@gmail.com>
2023-12-31 03:26:32 +0600

    header text added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -5,15 +5,15 @@ const Blog = () => {
     return (
         <ul className='blog-container'>
             <li>
-                <h2>What is the purpose of react router?</h2>
+                <h3>What is the purpose of react router?</h3>
                 <p>React Router simplifies navigation in React applications, offering a declarative approach to define routes and associated components. It supports nested and dynamic routing, integrates seamlessly with browser history, and enables code splitting for improved performance. With a component-based structure, it allows developers to organize views hierarchically. React Router also facilitates transition effects and provides navigation lifecycle hooks, enhancing user experience and control over route behaviors. Overall, React Router is crucial for building single-page applications, streamlining navigation, and optimizing the user interface in React-based web development.</p>
             </li>
             <li>
-                <h2>How does context api works?</h2>
+                <h3>How does context api works?</h3>
                 <p>The Context API in React provides a way to share state across components without prop drilling. It involves creating a context with `createContext` and using `Provider` to wrap components that need access to the shared state. Consumers access this state using `useContext` or by wrapping components with `Consumer`. This simplifies state management, especially in deeply nested components, enhancing code readability and maintainability by avoiding the need to pass props through intermediate components. The Context API streamlines global state handling, making it a powerful tool for managing shared data in a React application.</p>
             </li>
             <li>
-                <h2>What is useRef hook?</h2>
+                <h3>What is useRef hook?</h3>
                 <p>The `useRef` hook in React is a functional component feature that creates a mutable object called a "ref," persisting across renders without triggering re-renders. It's commonly used to access and interact with DOM elements directly, manage focus, or store mutable values that don't trigger component re-rendering. Unlike state, changes to a `useRef` value do not cause component re-renders, making it useful for scenarios where you need to maintain and modify values without affecting the rendering lifecycle. This hook is instrumental in handling imperative actions and maintaining state that shouldn't trigger a UI update in React applications.</p>
             </li>
         </ul>
--- src/Components/Topics/Topics.js
+++ src/Components/Topics/Topics.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Topics.css';
 
 const Topics = () => {
     return (
-        <div>
-            <p>this is about topics</p>
+        <div className='topic-container'>
+            <p>Quiz Genius is an engaging and intellectually stimulating online platform designed to challenge and entertain users with a diverse range of quizzes and trivia. At Quiz Genius, we believe in fostering a fun and interactive learning experience, providing a space where users can test their knowledge across various topics and compete with friends or fellow enthusiasts. The platform features an extensive collection of quizzes spanning science, history, pop culture, technology, and more, ensuring there's something for every curious mind. Our user-friendly interface and intuitive navigation powered by React technology make exploring and participating in quizzes a seamless and enjoyable process. Quiz Genius goes beyond traditional trivia, offering dynamic and thought-provoking questions that cater to both casual participants and avid quiz enthusiasts. Whether you're looking to challenge yourself or engage in friendly competition, Quiz Genius provides an inclusive and entertaining environment.</p>
         </div>
     );
 };
